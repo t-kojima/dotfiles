75ae8fe737527cdffaaa81557a6de0d1b2b6cebb
t-kojima <t-kojima@hokuichi-e.com>
2018-04-07 15:52:56 +0900

    add vscode setting.json

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # dotfiles
+
+## install
+
+```
+git clone https://github.com/t-kojima/dotfiles.git
+cd dotfiles
+./install.sh
+```
--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+# https://qiita.com/b4b4r07/items/b70178e021bef12cd4a2
 # https://qiita.com/b4b4r07/items/24872cdcbec964ce2178
 
 for f in .??*
